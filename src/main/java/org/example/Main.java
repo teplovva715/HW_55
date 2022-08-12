e1ddef6a281a6b8a9427ecdfb683b4cf967df435
Viktor <98766628+teplovva715@users.noreply.github.com>
2022-08-13 00:46:07 +0300

    Added HW_55

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+import org.apache.logging.log4j.*;
+
+public class Main {
+    private static Logger logger1 = LogManager.getLogger(Main.class);
+
+    public static void main(String[] args) {
+        logger1.trace("This is a trace message");
+        logger1.debug("This is a debug message");
+        logger1.info("This is an info message");
+        logger1.warn("This is a warn message");
+        logger1.error("This is an error message");
+        logger1.fatal("This is a fatal message");
+    }
+}
